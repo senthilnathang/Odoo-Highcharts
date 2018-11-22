72b8e79ef0ca52e689cf1d5006d763bad55a958f
Senthilnathan G <bornspy@gmail.com>
2018-11-22 20:03:45 +0530

    highcharts module

---
+++ web_highcharts/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Highcharts',
+    'version': '1.0',
+    'category': 'HR',
+    'summary': 'Highcharts',
+    'description': 'Highcharts Charting Library',
+    'depends': ['base'],
+    'data' : [
+    'views/assets.xml'],
+    'installable': True,
+    'auto_install': False,
+}
